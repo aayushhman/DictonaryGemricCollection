e1a75db9bded367d3ce062de73edbe27c8b8d7b9
aayushhman <aayushmandal696@gmail.com>
2025-01-31 21:38:03 +0545

    pratice dictionary collection

---
+++ DictonaryGemricCollection/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictonaryGemricCollection
+{
+    class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string designitaion { get; set; }
+        public int salry { get; set; }
+
+    }
+}
--- DictonaryGemricCollection/Program.cs
+++ DictonaryGemricCollection/Program.cs
@@ -10,55 +10,144 @@ namespace DictonaryGemricCollection
     {
         static void Main(string[] args)
         {
-            //Dictionary<string, string> mydict = new Dictionary<string, string>();
-            //mydict.Add("position", "location/places/situation/spot/site/locality/posture..");
-            //mydict.Add("respond", "answer/reply/riposte/rejoin/relitate..");
-            //mydict.Add("smile", "grin/gently laugh/giggle..");
-            //mydict.Add("someone else", "another man who is unkown..");
-            //mydict.Add("confide", "tell someone about a secret or private matter while trusting..");
-            //Console.WriteLine(mydict["position"]);
-            //foreach (KeyValuePair<string, string> item in mydict)
-            //{
-            //    Console.WriteLine("key is:" + item.Key + "value is:" + item.Value);
-            //}
 
-            //Dictionary<float, string> mydict2 = new Dictionary<float, string>();
-            //mydict2.Add(6.00f, "social study book..");
-            //mydict2.Add(6.30f, "computer book..");
-            //mydict2.Add(7.10f, "english book..");
-            //mydict2.Add(7.40f, "nepali book..");
-            //mydict2.Add(8.10f, "science book..");
-            //mydict2.Add(8.40f, "opt math book..");
-            //mydict2.Add(9.10f, "Math book..");
-            //foreach (var item in mydict2)
-            //{
-            //    Console.WriteLine(item);
-            //}
 
-            //foreach ( string key in mydict.Keys)
+            //Employee emp1 = new Employee()
             //{
-            //    Console.WriteLine(key);
-            //}
-            //Console.WriteLine("_________________________________________________________________");
-            //foreach (string value in mydict.Values)
+            //    id=111,
+            //    name="Aayush",
+            //    designitaion="Manager",
+            //    salry=18000
+            //};
+            //Employee emp2 = new Employee()
             //{
-            //    Console.WriteLine(value);
-            //}
+            //    id = 222,
+            //    name = "Kumar",
+            //    designitaion = "Assitant",
+            //    salry = 12000
+            //};
+            //Employee emp3 = new Employee()
+            //{
+            //    id = 333,
+            //    name = "Mandal",
+            //    designitaion = "Operator",
+            //    salry = 8000
+            //};
+            //Dictionary<int, Employee> myemployees = new Dictionary<int, Employee>();
+            //myemployees.Add(emp1.id , emp1);
+            //myemployees.Add(emp2.id, emp2);
+            //myemployees.Add(emp3.id, emp3);
 
-            Dictionary<TimeSpan, string> mydict2 = new Dictionary<TimeSpan, string>();
-            mydict2.Add(new TimeSpan(6, 0, 0), "social study book");
-            mydict2.Add(new TimeSpan(6, 35, 0), "computer book");
-            mydict2.Add(new TimeSpan(7, 5, 0), "english book");
-            mydict2.Add(new TimeSpan(7, 35, 0), "nepali book");
-            mydict2.Add(new TimeSpan(8, 5, 0), "science book");
-            mydict2.Add(new TimeSpan(8, 35, 0), "opt math  book");
-            mydict2.Add(new TimeSpan(9, 5, 0), "math book");
-            Console.WriteLine("class subject routine...");
-            Console.WriteLine("---------------------------------");
+
+            //Console.WriteLine(myemployees.Count(emp => emp.Value.name.StartsWith("A")));
+
+
+
+
+
+
+
+
+            Dictionary<string, string> mydict = new Dictionary<string, string>();
+            mydict.Add("position", "location/places/situation/spot/site/locality/posture..");
+            mydict.Add("respond", "answer/reply/riposte/rejoin/relitate..");
+            mydict.Add("smile", "grin/gently laugh/giggle..");
+            mydict.Add("someone else", "another man who is unkown..");
+            mydict.Add("confide", "tell someone about a secret or private matter while trusting..");
+
+            string value;
+            if (mydict.TryGetValue("smle", out value))
+            {
+                Console.WriteLine(value);
+            }
+            else
+            {
+                Console.WriteLine("Value not found!!");
+            }
+
+
+            mydict.TryGetValue("smile", out value);
+            Console.WriteLine(value);
+
+            if (mydict.ContainsValue("another man who is unkown.."))
+            {
+                Console.WriteLine("Value Found!!");
+            }
+            else
+            {
+                Console.WriteLine("Value not found!!");
+            }
+
+
+
+            Console.WriteLine(mydict.ContainsValue("another man who is unkown.."));
+
+            if (mydict.ContainsKey("smie"))
+            {
+                Console.WriteLine("key Found!!");
+            }
+            else
+            {
+                Console.WriteLine("Key not found!!");
+            }
+
+            Console.WriteLine(mydict.ContainsKey("smile"));
+
+            foreach (KeyValuePair<string, string> item in mydict)
+            {
+                Console.WriteLine("key is:" + item.Key + "value is:" + item.Value);
+            }
+            mydict.Remove("smile");
+            Console.WriteLine("-------------------------------------");
+            foreach (KeyValuePair<string, string> item in mydict)
+            {
+                Console.WriteLine("key is:" + item.Key + "value is:" + item.Value);
+            }
+
+            Console.WriteLine(mydict.Count);
+            Console.WriteLine(mydict["position"]);
+            foreach (KeyValuePair<string, string> item in mydict)
+            {
+                Console.WriteLine("key is:" + item.Key + "value is:" + item.Value);
+            }
+
+            Dictionary<float, string> mydict2 = new Dictionary<float, string>();
+            mydict2.Add(6.00f, "social study book..");
+            mydict2.Add(6.30f, "computer book..");
+            mydict2.Add(7.10f, "english book..");
+            mydict2.Add(7.40f, "nepali book..");
+            mydict2.Add(8.10f, "science book..");
+            mydict2.Add(8.40f, "opt math book..");
+            mydict2.Add(9.10f, "Math book..");
             foreach (var item in mydict2)
             {
-                Console.WriteLine($"{item.Key:hh\\:mm} AM - {item.Value}");
+                Console.WriteLine(item);
             }
+
+            foreach (string key in mydict.Keys)
+            {
+                Console.WriteLine(key);
+            }
+            Console.WriteLine("_________________________________________________________________");
+            foreach (string value in mydict.Values)
+            {
+                Console.WriteLine(value);
+            }
+
+            ////Dictionary<TimeSpan, string> mydict2 = new Dictionary<TimeSpan, string>();
+            ////mydict2.Add(new TimeSpan(6, 0, 0), "social study book");
+            ////mydict2.Add(new TimeSpan(6, 35, 0), "computer book");
+            ////mydict2.Add(new TimeSpan(7, 5, 0), "english book");
+            ////mydict2.Add(new TimeSpan(7, 35, 0), "nepali book");
+            ////mydict2.Add(new TimeSpan(8, 5, 0), "science book");
+            ////mydict2.Add(new TimeSpan(8, 35, 0), "opt math  book");
+            ////mydict2.Add(new TimeSpan(9, 5, 0), "math book");
+            ////Console.WriteLine("class subject routine...");
+            ////Console.WriteLine("---------------------------------");
+            ////foreach (var item in mydict2)
+            ////{
+            ////    Console.WriteLine($"{item.Key:hh\\:mm} AM - {item.Value}");
+            ////}
 
 
             Console.ReadLine();
